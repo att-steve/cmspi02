d30345839e399c978d2a7ac652ccef7787e9abf0
stevehanlon <steve@hanlon.co.uk>
2020-03-06 07:33:37 +0000

    cleaned up imports

--- covid.py
+++ covid.py
@@ -1,8 +1,8 @@
-import plotly.express as px
-
-import pandas as pd
 import io
 import requests
+import pandas as pd
+import plotly.express as px
+
 url="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
 s=requests.get(url).content
 df=pd.read_csv(io.StringIO(s.decode('utf-8')))
